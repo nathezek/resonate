7de202c00131341647555d209945488382ffce12
nathezek <nathezek7@gmail.com>
2026-01-19 22:14:56 +0300

    feat: Motion for the layout 💨

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,32 @@
+import { AnimatePresence, motion } from "motion/react";
 import Navbar from "./components/main/navbar";
 
-function App() {
+// state imports
+import { useState } from "react";
 
+function App() {
+    const [session, setSession] = useState<boolean>(false);
+    const handleToggle = () => setSession((prev) => !prev);
     return (
-        <>
-            <Navbar />
-        </>
-    )
+        <main className="flex flex-col items-center justify-center">
+            <AnimatePresence mode="wait">
+                {session ? (
+                    <Navbar key="navbar-unique" toggleSession={handleToggle} />
+                ) : (
+                    <motion.div
+                        key="start-button"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className="cursor-pointer"
+                        onClick={() => setSession(true)}
+                    >
+                        Start Session
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </main>
+    );
 }
 
 export default App;
--- apps/web/src/components/main/navbar.tsx
+++ apps/web/src/components/main/navbar.tsx
@@ -1,38 +1,76 @@
-import { IconLineDotted, IconMicrophone, IconPlayerStop } from "@tabler/icons-react";
+import {
+    IconLineDotted,
+    IconMicrophone,
+    IconMicrophoneOff,
+    IconPlayerStop,
+} from "@tabler/icons-react";
+import { useState } from "react";
 
-const Navbar = () => {
+import { motion } from "motion/react";
+
+type T_NAVBAR = {
+    toggleSession: () => void;
+};
+
+const Navbar = ({ toggleSession }: T_NAVBAR) => {
     return (
         <nav className="w-full absolute top-0 left-0 right-0 p-4 h-fit flex items-center justify-between">
             <div />
-            <SessionControls />
+            <SessionControls onEndSession={toggleSession} />
             <Profile />
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar;
 
 // =================================== SUB-COMPONENTS ======================================= //
 
-const SessionControls = () => {
+const SessionControls = ({ onEndSession }: { onEndSession: () => void }) => {
+    const [mic, toggleMic] = useState<boolean>(false);
+    const tapVariants = {
+        tapped: { scale: 0.65, opacity: 0.8 }, // Adjusted opacity to 0.8; 10 is usually too high
+    };
     return (
-        <div className="w-64 h-12 bg-neutral-800 text-neutral-100 rounded-2xl px-1.5 flex items-center justify-between py-1.5">
-            <span className="h-full w-fit gap-x-2.5 pl-2 flex items-center justify-center">
-                <IconMicrophone size={20} />
+        <motion.div
+            key={"session-bar"}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            className="w-64 h-12 bg-neutral-800 text-neutral-100 rounded-2xl cursor-default px-1.5 select-none flex items-center justify-between py-1.5"
+        >
+            <span
+                className="h-full w-fit gap-x-2.5 pl-2.5 flex items-center justify-center"
+                onClick={() => toggleMic(!mic)}
+            >
+                <motion.span whileTap={"tapped"} className="cursor-pointer">
+                    {!mic ? (
+                        <motion.span variants={tapVariants}>
+                            <IconMicrophone size={20} />
+                        </motion.span>
+                    ) : (
+                        <motion.span variants={tapVariants}>
+                            <IconMicrophoneOff
+                                size={20}
+                                className="text-red-400"
+                            />
+                        </motion.span>
+                    )}
+                </motion.span>
                 <span className="text-neutral-700">|</span>
                 <IconLineDotted size={32} />
             </span>
             <span className="tracking-tight mono text-[0.895rem]">08:04</span>
-            <span className="h-full w-12 flex items-center justify-center rounded-xl bg-neutral-100 text-neutral-800">
+            <span
+                onClick={onEndSession}
+                className="cursor-pointer h-full w-12 flex items-center justify-center rounded-xl bg-neutral-100 text-neutral-800"
+            >
                 <IconPlayerStop size={18} />
             </span>
-        </div>
-    )
-}
-
+        </motion.div>
+    );
+};
 
 const Profile = () => {
-    return (
-        <div className="w-10 h-10 bg-neutral-100 rounded-full"></div>
-    )
-}
+    return <div className="w-10 h-10 bg-neutral-100 rounded-full"></div>;
+};
