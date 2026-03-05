619809478bf7cfcd4b986c3fe90f206bef58857a
nathezek <nathezek7@gmail.com>
2026-03-05 10:18:00 +0300

    feat: implement 💻️ interaction with agent ui.
    - added tailwindcss
    - removed biolerplate
    - implemented sendMessage function to send
      user message.
    - created an area to show agent_response.data

--- apps/web_interface/src/App.tsx
+++ apps/web_interface/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react';
 import './App.css'
 
+type AGENT_MESSAGE = {
+    data: string;
+    type: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [input, setInput] = useState('');
+    const [agent_response, setAgentResponse] = useState<AGENT_MESSAGE>();
+    const ws = useRef < WebSocket | null>(null);
+
+    useEffect(() => {
+        ws.current = new WebSocket("ws://localhost:3001/ws");
+        ws.current.onmessage = (event) => {
+            const data : AGENT_MESSAGE = JSON.parse(event.data);
+            console.log("Agent:", data);
+            setAgentResponse(data);
+
+            return () => {
+                ws.current?.close();
+            };
+        };
+    }, []);
+
+    const sendMessage = (e: React.SubmitEvent) => {
+        e.preventDefault();
+        ws.current?.send(input);
+        setInput('');
+    };
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <>
+          <form onSubmit={sendMessage}>
+              <input type="text" placeholder='Write a message...' value={input} onChange={(e) => setInput(e.target.value)} />
+              <button>Send</button>
+          </form>
+       <h1>Agent response</h1>
+          <span className='lg:max-w-3xl'>
+              {agent_response?.data}
+          </span>
     </>
   )
 }
 
-export default App
+export default App;
