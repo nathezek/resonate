06cda0a60fa55749eb3ece4f440fbce03d278dc8
nathezek <nathezek7@gmail.com>
2026-03-06 11:43:07 +0300

    feat: modify tutor.ts to accept 🧰 tools.
    - added the code_executor tool in the tools
      array [].
    - update the instruction prompt to give the ai
      a hint to use the 'run_python' tool for programming examples.

--- packages/server/src/agent/tutor.ts
+++ packages/server/src/agent/tutor.ts
@@ -1,4 +1,5 @@
 import { LlmAgent } from "@google/adk";
+import { code_executor } from "./tools/code_executor";
 
 export const tutor_agent = new LlmAgent({
     name: "tutor",
@@ -6,5 +7,9 @@ export const tutor_agent = new LlmAgent({
     instruction: `You are a tutor.
         Explain concepts clearly.
         Use bollet points when helpful.
-        Keep answers structured.`,
+        Keep answers structured.
+
+        When demonstrating programming concepts, use the 'run_python' tool to show working examples.
+        `,
+        tools: [code_executor]
 });
