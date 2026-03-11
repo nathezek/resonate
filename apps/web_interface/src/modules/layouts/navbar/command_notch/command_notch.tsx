e6369e25d102434d3be8959ce2b5efaef3a92617
nathezek <nathezek7@gmail.com>
2026-03-11 08:53:44 +0300

    feat: Make user's current 💬 message display in command_notch instead of chat_history.

--- apps/web_interface/src/modules/layouts/navbar/command_notch/command_notch.tsx
+++ apps/web_interface/src/modules/layouts/navbar/command_notch/command_notch.tsx
@@ -11,6 +11,7 @@ import { MicVisualizer } from "./components/mic_visualizer";
 import { useSession } from "../../../../stores/session_store";
 import Timer from "./components/timer";
 import { useKeyboard } from "../../../../stores/keyboard_store";
+import CurrentUserMessageDisplayer from "./components/current_user_message_displayer";
 
 export const CommandNotch = () => {
     const {
@@ -24,52 +25,56 @@ export const CommandNotch = () => {
 
     const { is_keyboard_enabled, toggle_keyboard } = useKeyboard();
     return (
-        <div className="flex bg-neutral-200 dark:bg-neutral-800 select-none text-neutral-700 items-center justify-between w-full h-12 p-1.5 lg:w-xl rounded-2xl">
-            {/*----- Left ----*/}
-            <div className="flex h-full items-center gap-x-1">
-                <button onClick={toggle_session_pause}>
-                    {is_session_paused ? (
-                        <IconPlayerPlayFilled size={18} />
-                    ) : (
-                        <IconPlayerPause size={18} />
-                    )}
-                </button>
-                <span className="opacity-40">|</span>
-                <button onClick={toggle_keyboard}>
-                    {is_keyboard_enabled ? (
-                        <IconKeyboard size={22} />
-                    ) : (
-                        <IconKeyboardOff size={22} />
-                    )}
-                </button>
-                <span className="opacity-40">|</span>
-                <button onClick={toggle_mute}>
-                    {is_mic_muted ? (
-                        <IconMicrophoneOff size={18} />
-                    ) : (
-                        <IconMicrophone size={18} />
-                    )}
-                </button>
+        <div className="w-fit h-fit flex flex-col items-center bg-neutral-200 dark:bg-neutral-800 text-neutral-700 rounded-2xl p-1.5 ">
+            <div className="flex select-none items-center justify-between gap-x-8 w-full h-fit mb-1 lg:w-xl">
+                {/*----- Left ----*/}
+                <div className="flex h-full items-center gap-x-1">
+                    <button onClick={toggle_session_pause}>
+                        {is_session_paused ? (
+                            <IconPlayerPlayFilled size={18} />
+                        ) : (
+                            <IconPlayerPause size={18} />
+                        )}
+                    </button>
+                    <span className="opacity-40">|</span>
+                    <button onClick={toggle_keyboard}>
+                        {is_keyboard_enabled ? (
+                            <IconKeyboard size={22} />
+                        ) : (
+                            <IconKeyboardOff size={22} />
+                        )}
+                    </button>
+                    <span className="opacity-40">|</span>
+                    <button onClick={toggle_mute}>
+                        {is_mic_muted ? (
+                            <IconMicrophoneOff size={18} />
+                        ) : (
+                            <IconMicrophone size={18} />
+                        )}
+                    </button>
+                </div>
+
+                {/*---- Center ----*/}
+                <MicVisualizer
+                    isActive={
+                        (session_status === "active" && is_mic_muted) ||
+                        is_session_paused
+                    }
+                />
+
+                {/*----- Right ------*/}
+                <div className="flex h-full items-center gap-x-4">
+                    <Timer />
+                    <button
+                        onClick={() => end_session()}
+                        className="text-neutral-100 w-14 rounded-xl flex items-center justify-center bg-neutral-700"
+                    >
+                        <IconPlayerStop size={18} />
+                    </button>
+                </div>
             </div>
 
-            {/*---- Center ----*/}
-            <MicVisualizer
-                isActive={
-                    (session_status === "active" && is_mic_muted) ||
-                    is_session_paused
-                }
-            />
-
-            {/*----- Right ------*/}
-            <div className="flex h-full items-center gap-x-4">
-                <Timer />
-                <button
-                    onClick={() => end_session()}
-                    className="text-neutral-100 w-14 rounded-xl flex items-center justify-center bg-neutral-700"
-                >
-                    <IconPlayerStop size={18} />
-                </button>
-            </div>
+            <CurrentUserMessageDisplayer />
         </div>
     );
 };
