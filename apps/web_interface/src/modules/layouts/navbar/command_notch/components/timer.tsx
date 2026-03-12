097b8b4e2a8fb0c5e756f4c0a6680ae0615d8129
nathezek <nathezek7@gmail.com>
2026-03-12 08:27:51 +0300

    chore: 🕚️ Timer code cleanup.

--- apps/web_interface/src/modules/layouts/navbar/command_notch/components/timer.tsx
+++ apps/web_interface/src/modules/layouts/navbar/command_notch/components/timer.tsx
@@ -1,13 +1,10 @@
 import { useEffect } from "react";
-import { useSession } from "../../../../../stores/session_store";
 import { useSessionTimer } from "../../../../../stores/session_timer_store";
 
 const Timer = () => {
-    const { session_start_time, is_session_paused } = useSession();
     const { is_running, increment, get_formatted_time } = useSessionTimer();
 
     useEffect(() => {
-        if (!session_start_time || is_session_paused) return;
         let interval: ReturnType<typeof setInterval> | null = null;
 
         if (is_running) {
@@ -19,7 +16,7 @@ const Timer = () => {
         return () => {
             if (interval) clearInterval(interval!);
         };
-    }, [is_running, increment, is_session_paused]);
+    }, [is_running, increment]);
 
     return (
         <span className="text-neutral-400 text-sm mr-4">
